495dfdd413313409ed86c4621a7da1d47619bf6d
MUTESA20 <mutesaaj@gmail.com>
2023-11-28 09:24:41 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,3 +1,6 @@
-def add(a, b):
-    """Return the addition of a and b."""
-    return (a + b)
+#!/usr/bin/env python3
+add = __import__('10-add').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
